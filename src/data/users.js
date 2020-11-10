69aa15ccd38e15c697918a0c4127b42a976f8d57
GabrielCdev <gabi_borges01@hotmail.com>
2020-11-10 15:25:52 -0300

    Criando arquivos de usuarios

---
+++ src/data/users.js
@@ -0,0 +1,26 @@
+export default [
+    {
+        id: 1,
+        name: 'João Pedro',
+        email: 'jpedro@email.com',
+        avatarUrl: 'https://cdn.pixabay.com/photo/2013/07/13/10/07/man-156584_960_720.png'
+    },
+    {
+        id: 2,
+        name: 'Maria Clara',
+        email: 'claramaria@email.com',
+        avatarUrl: 'https://cdn.pixabay.com/photo/2014/04/03/10/32/user-310807_960_720.png'
+    },
+    {
+        id: 3,
+        name: 'Gabriel Pensador',
+        email: 'pensadorgabriel@email.com',
+        avatarUrl: 'https://cdn.pixabay.com/photo/2014/04/03/10/32/businessman-310819_960_720.png'
+    },
+    {
+        id: 4,
+        name: 'Mariana Santiago',
+        email: 'marianasant@email.com',
+        avatarUrl: 'https://cdn.pixabay.com/photo/2016/04/26/07/20/woman-1353803_960_720.png'
+    },
+];
