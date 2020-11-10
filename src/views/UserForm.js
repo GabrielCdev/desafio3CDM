9231f2dc7b145326ef35acb5c66b641fd629a004
GabrielCdev <gabi_borges01@hotmail.com>
2020-11-10 15:59:44 -0300

    Fazendo formulario para usuarios.

--- src/views/UserForm.js
+++ src/views/UserForm.js
@@ -1,10 +1,50 @@
 import React, { useState } from 'react'
-import { Text } from 'react-native'
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native'
 
-export default ({ route, navigtion }) => {
+export default ({ route, navigation }) => {
     const [user, setUser] = useState(route.params ? route.params : {})
 
     return (
-        <Text>{user.id}</Text>
+        <View style={style.form}>
+            <Text>Nome</Text>
+            <TextInput
+                style={style.input}
+                onChangeText={name => setUser({...user, name})}
+                placeholder="Nome completo"
+                value={user.name}
+            />
+            <Text>E-mail</Text>
+            <TextInput
+                style={style.input}
+                onChangeText={email => setUser({...user, email})}
+                placeholder="E-mail: email@email.com"
+                value={user.email}
+            />
+            <Text>Avatar (URL)</Text>
+            <TextInput
+                style={style.input}
+                onChangeText={avatarUrl => setUser({...user, avatarUrl})}
+                placeholder="Insira a URL do avatar."
+                value={user.avatarUrl}
+            />
+            <Button 
+                title="Salvar"
+                onPress={() => {
+                    navigation.goBack()
+                }}
+            />
+        </View>
     )
 }
+
+const style = StyleSheet.create({
+    form: {
+        padding: 12
+    },
+    input: {
+        height: 40,
+        borderColor: 'gray',
+        borderWidth: 1,
+        marginBottom: 15,
+    }
+})
