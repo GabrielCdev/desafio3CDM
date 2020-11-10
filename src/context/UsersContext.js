1f55e9ab58bcf0873ffbdb671f0f3169021d97d6
GabrielCdev <gabi_borges01@hotmail.com>
2020-11-10 16:44:35 -0300

    Fazendo a atualizaçao de dados.

--- src/context/UsersContext.js
+++ src/context/UsersContext.js
@@ -15,6 +15,15 @@ const actions = {
         }
     },
 
+    updateUser(state, action) {
+        const updated = action.payload
+        
+        return {
+            ...state,
+            users: state.users.map(u => u.id === updated.id ? updated : u)
+        }
+    },
+
     deleteUser(state, action) {
         const user = action.payload
 
